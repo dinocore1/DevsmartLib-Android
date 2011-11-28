de8946fc0b020764919e514c95ce19853432942b
Paul Soucy <paul@dev-smart.com>
2011-11-27 19:48:21 -0500

    added clickable links to StringUtils

--- devsmartlib/src/com/devsmart/android/StringUtils.java
+++ devsmartlib/src/com/devsmart/android/StringUtils.java
@@ -8,6 +8,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.content.res.Resources;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class StringUtils {
 	
@@ -64,5 +72,32 @@ public class StringUtils {
 		  retval.toArray(retarray);
 		  return retarray;
 	  }
+	  
+	  public abstract static class ClickSpan extends ClickableSpan {
+		  
+	  }
+	  public static void clickify(TextView view, final String clickableText, 
+			    final ClickSpan span) {
+
+			    CharSequence text = view.getText();
+			    String string = text.toString();
+
+			    int start = string.indexOf(clickableText);
+			    int end = start + clickableText.length();
+			    if (start == -1) return;
+
+			    if (text instanceof Spannable) {
+			        ((Spannable)text).setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+			    } else {
+			        SpannableString s = SpannableString.valueOf(text);
+			        s.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+			        view.setText(s);
+			    }
+
+			    MovementMethod m = view.getMovementMethod();
+			    if ((m == null) || !(m instanceof LinkMovementMethod)) {
+			        view.setMovementMethod(LinkMovementMethod.getInstance());
+			    }
+			}
 
 }
